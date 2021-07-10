b05a846d102485a253c21399477cfd354b411f95
Hugh <Hugh.blayney@gmail.com>
2021-07-10 20:40:24 +0100

    Reflected ray function

--- internal/pkgs/objects/objects.go
+++ internal/pkgs/objects/objects.go
@@ -10,3 +10,10 @@ type Object interface {
 	Normal(vectors.Vector) vectors.Vector
 	Reflect(rays.Ray) rays.Ray
 }
+
+// I don't think this is done correctly, but this is the best way I could think of.
+// I don't want to be duplicating code, and this function is common among all shapes.
+func ComputeReflectedRay(incoming_ray rays.Ray, point_of_intersection *vectors.Vector, surface_normal *vectors.Vector) rays.Ray {
+	new_direction := surface_normal.MultiplyScalar(2 * incoming_ray.Dot(surface_normal)).Subtract(incoming_ray.Direction.MultiplyScalar(-1))
+	return rays.Ray{Origin: point_of_intersection, Direction: new_direction}
+}
