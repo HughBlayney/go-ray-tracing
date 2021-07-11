0b6f96083e83fdbe0cfffa59ce653e38ecedb5af
Hugh <Hugh.blayney@gmail.com>
2021-07-11 16:50:52 +0100

    Temporarily move position to parent class

--- internal/pkgs/lights/lights.go
+++ internal/pkgs/lights/lights.go
@@ -2,9 +2,12 @@ package lights
 
 import (
 	"image/color"
+
+	"github.com/HughBlayney/go-ray-tracing/internal/pkgs/vectors"
 )
 
 type Light struct {
 	Color     color.RGBA
-	Intensity float64 // [0.0, 1.0]
+	Intensity float64        // [0.0, 1.0]
+	Position  vectors.Vector // temporary
 }
--- internal/pkgs/lights/point.go
+++ internal/pkgs/lights/point.go
@@ -1,10 +1,5 @@
 package lights
 
-import (
-	"github.com/HughBlayney/go-ray-tracing/internal/pkgs/vectors"
-)
-
 type PointLight struct {
-	Position vectors.Vector
 	Light
 }
