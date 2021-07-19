567bf5b757c4bcd8a0f9d9d2ea8cb0feed46c870
Hugh <Hugh.blayney@gmail.com>
2021-07-19 19:01:23 +0100

    Tests for reflect

--- internal/pkgs/vectors/vectors_test.go
+++ internal/pkgs/vectors/vectors_test.go
@@ -1,6 +1,7 @@
 package vectors
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/HughBlayney/go-ray-tracing/internal/pkgs/utils"
@@ -104,6 +105,60 @@ func TestVector_Normalise(t *testing.T) {
 			v.Normalise()
 			if got := v.Magnitude(); !(utils.Close_enough(got, tt.want)) {
 				t.Errorf("After normalisation, Vector.Magnitude() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestVector_Reflect(t *testing.T) {
+	type fields struct {
+		X float64
+		Y float64
+		Z float64
+	}
+	type args struct {
+		surface_normal *Vector
+	}
+	tests := []struct {
+		name                 string
+		fields               fields
+		args                 args
+		wantReflected_vector *Vector
+	}{
+		{
+			name:   "Straight back",
+			fields: fields{0.0, -1.0, 0.0},
+			args: args{
+				surface_normal: &Vector{0.0, 1.0, 0.0},
+			},
+			wantReflected_vector: &Vector{0.0, 1.0, 0.0},
+		},
+		{
+			name:   "No reflection",
+			fields: fields{1.0, 0.0, 0.0},
+			args: args{
+				surface_normal: &Vector{0.0, 1.0, 0.0},
+			},
+			wantReflected_vector: &Vector{1.0, 0.0, 0.0},
+		},
+		{
+			name:   "No reflection",
+			fields: fields{1.0, 0.0, 0.0},
+			args: args{
+				surface_normal: &Vector{0.0, 1.0, 0.0},
+			},
+			wantReflected_vector: &Vector{1.0, 0.0, 0.0},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			v := &Vector{
+				X: tt.fields.X,
+				Y: tt.fields.Y,
+				Z: tt.fields.Z,
+			}
+			if gotReflected_vector := v.Reflect(tt.args.surface_normal); !reflect.DeepEqual(gotReflected_vector, tt.wantReflected_vector) {
+				t.Errorf("Vector.Reflect() = %v, want %v", gotReflected_vector, tt.wantReflected_vector)
 			}
 		})
 	}
